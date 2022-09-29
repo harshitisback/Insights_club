e3d5c9fdc91870066c6f2167330f4bf00ef4c09c
harshit <harshit.tiwari2020@vitbhopal.ac.in>
2022-09-29 11:58:50 +0530

    auth done

--- app/src/main/java/com/example/menu/article_description_screen.java
+++ app/src/main/java/com/example/menu/article_description_screen.java
@@ -9,6 +9,8 @@ public class article_description_screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_article_description_screen);
     }
 }
